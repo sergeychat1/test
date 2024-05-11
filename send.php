67189de68de199d08970b074e20e4010c8684ae6
Sergey <72626933+sergeychat1@users.noreply.github.com>
2024-05-11 20:39:33 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,71 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Пошта:</b> $mail<br><br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str; };
+
+    // Настройки вашей почты
+    $mail->Host = 'smtp-relay.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username = 'mr.sokolsergey@gmail.com'; // Логин на почте
+    $mail->Password = '123321123As'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    $mail->Port = 465;
+    $mail->setFrom('mr.sokolsergey@gmail.com', 'Тест Тест'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('mr.sokolsergey@gmail.com');
+    //$mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+    } else {
+        $result = "error";
+    }
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+//echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header('Location: index.html');
